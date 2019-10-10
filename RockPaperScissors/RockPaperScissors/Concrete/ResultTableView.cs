143774be6997976a251766d4f163e1fe58547c6f
Iljaz-PC\Iljaz <tuma.iljaz@gmail.com>
2019-10-10 22:20:49 +0200

    added ResultTableView class

---
+++ RockPaperScissors/RockPaperScissors/Concrete/ResultTableView.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors.Concrete
+{
+    public class ResultTableView
+    {
+        public int Round { get; set; }
+        public GameObject PlayerOneObject { get; set; } //PlayerOne chooise
+        public GameObject PlayerTwoObject { get; set; } //PlayerTwo chooise
+        public Player WinnerPlayer { get; set; }
+        public GameResult WinnerObject { get; set; }
+
+        public ResultTableView(int round,GameObject playerOneObject,GameObject playerTwoObject)
+        {
+            Round = round;
+            PlayerOneObject = playerOneObject;
+            PlayerTwoObject = playerTwoObject;
+            WinnerObject =(GameResult)Rule.GetWinnerObject(playerOneObject, playerTwoObject);
+
+            if (WinnerObject == GameResult.Draw)
+                WinnerPlayer = Player.NoOne;
+            else if (WinnerObject == (GameResult)playerOneObject)
+                WinnerPlayer = Player.PlayerOne;
+            else
+                WinnerPlayer = Player.PlayerTwo;
+        }
+    }
+}
