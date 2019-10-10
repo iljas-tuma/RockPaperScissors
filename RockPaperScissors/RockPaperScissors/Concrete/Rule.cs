19e14835c554ead8b7fa2b35d0facef71b981c0d
Iljaz-PC\Iljaz <tuma.iljaz@gmail.com>
2019-10-10 22:12:13 +0200

    addes Rule class

---
+++ RockPaperScissors/RockPaperScissors/Concrete/Rule.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace RockPaperScissors.Concrete
+{
+    public static class Rule 
+    {
+        //winner scenarios
+        public static GameObject[] RockScissors = new GameObject[] { GameObject.Rock, GameObject.Scissors };    //Rock crushes Scrssors
+        public static GameObject[] ScissorsPaper = new GameObject[] { GameObject.Scissors, GameObject.Paper };  //Scissors cuts Paper
+        public static GameObject[] PaperRock = new GameObject[] { GameObject.Paper, GameObject.Rock };          //Paper covers Rock
+
+
+        public static GameResult GetWinnerObject(GameObject objectOne, GameObject objectTwo)
+        {
+            GameResult result = GameResult.Draw;                                                    //if objectOne==objectTwo
+
+            if(objectOne!=objectTwo)
+            {
+                if (RockScissors.Contains(objectOne) && RockScissors.Contains(objectTwo))           //match(Rock-Scissors, Scissors-Rock), the winner=Rock
+                    result = GameResult.Rock;
+                else if (ScissorsPaper.Contains(objectOne) && ScissorsPaper.Contains(objectTwo))    //match(Scissors-Paper, Paper-Scissors), the winner=Scissors
+                    result = GameResult.Scissors;
+                else if (PaperRock.Contains(objectOne) && PaperRock.Contains(objectTwo))            //match(Paper-Rock, Rock-Paper), the winner=Paper
+                    result = GameResult.Paper;
+            }
+            return result;
+        }
+    }
+}
