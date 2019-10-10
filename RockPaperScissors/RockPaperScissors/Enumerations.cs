02047b9d5324e23bb0e8cfb860194c20e0b32a70
Iljaz-PC\Iljaz <tuma.iljaz@gmail.com>
2019-10-10 21:56:49 +0200

    init commit

---
+++ RockPaperScissors/RockPaperScissors/Enumerations.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace RockPaperScissors
+{
+    //type of hand shape
+    public enum GameObject
+    {
+        Rock,Paper,Scissors
+    }
+    public enum GameResult
+    {
+        Rock,
+        Paper,
+        Scissors,
+        Draw //when the both players threw the same object
+    }
+    public enum Player
+    {
+        PlayerOne,
+        PlayerTwo,
+        NoOne //when the both players threw the same object
+    }
+}
